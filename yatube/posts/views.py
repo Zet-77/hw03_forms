c52adc9c3f351fe1839b83823a26b8a746bf065e
Azimov Zaur <aminamumina2013@gmail.com>
2022-12-12 20:20:39 +0300

    2review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,57 +7,34 @@ from .forms import PostForm
 from .utils import get_pagin
 
 def index(request):
-    page_obj = Post.objects.all()
-    paginator = Paginator(page_obj, 10) 
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        'page_obj': page_obj,
-    } 
+    context = get_pagin(Post.objects.all(), request)
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     post_list = group.posts.all()
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'title': 'Здесь будет информация о группах проекта Yatube',
-        'page_obj': page_obj
+        'post_list': post_list
     }
+    context.update(get_pagin(group.posts.all(), request))
     return render(request, 'posts/group_list.html', context)
 
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
-    user_posts = author.posts.all()
-    post_count = user_posts.count()
-    paginator = Paginator(user_posts, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    title = f'Профаил пользователя {username}'
     context = {
-        'title': title,
-        'page_obj': page_obj,
-        'post_count': post_count,
         'author': author,
     }
+    context.update(get_pagin(author.posts.all(), request))
     return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    author_post = post.author
-    post_count = Post.objects.filter(author=author_post).count()
-    title = f'Пост {post.text[0:30]}'
     context = {
         'post': post,
-        'post_count': post_count,
-        'title': title,
-        'author_post': author_post,
     }
     return render(request, 'posts/post_detail.html', context)
 
@@ -67,34 +44,27 @@ def post_create(request):
     is_edit = False
     form = PostForm(request.POST or None)
     context = {
-        'form': form,
-        'is_edit': is_edit,
+        'form': form
     }
-    if request.method == 'POST':
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('posts:profile', post.author)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', post.author)
     return render(request, 'posts/post_create.html', context)
 
 
 @login_required
 def post_edit(request, post_id):
-    is_edit = True
-    form = PostForm(request.POST)
-    post = get_object_or_404(Post, id=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     if post.author != request.user:
-        return redirect('posts:post_detail', post.id)
+        return redirect('posts:post_detail', post.pk)
     form = PostForm(request.POST or None, instance=post)
     if form.is_valid():
-        post = form.save(commit=False)
-        post.author = request.user
-        post.save()
-        return redirect('posts:post_detail', post.id)
+        form.save()
+        return redirect('posts:post_detail', post.pk)
     context = {
         'form': form,
-        'is_edit': is_edit,
         'post': post,
     }
     return render(request, 'posts/post_create.html', context)
