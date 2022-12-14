7d818b9597d35f29ceee11ee4fedab58ad47607c
Azimov Zaur <aminamumina2013@gmail.com>
2022-12-14 23:18:14 +0300

    2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -55,13 +55,13 @@ def post_create(request):
 
 @login_required
 def post_edit(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     if post.author != request.user:
-        return redirect('posts:post_detail', post.id)
+        return redirect('posts:post_detail', post.pk)
     form = PostForm(request.POST or None, instance=post)
     if form.is_valid():
         form.save()
-        return redirect('posts:post_detail', post.id)
+        return redirect('posts:post_detail', post.pk)
     context = {
         'form': form,
     }
