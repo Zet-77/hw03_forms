62f1c7818efa90176e92ef582b24d2230ddeeb4f
Azimov Zaur <aminamumina2013@gmail.com>
2022-12-17 11:31:33 +0300

    2

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -28,5 +28,7 @@ class GroupAdmin(admin.ModelAdmin):
     list_filter = ('title',)
     empty_value_display = '-пусто-'
 
+
 admin.site.register(Post, PostAdmin,)
 admin.site.register(Group, GroupAdmin,)
+
