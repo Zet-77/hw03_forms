7e7969bef8bdade1c17561dc3bb192599ac2997e
Azimov Zaur <aminamumina2013@gmail.com>
2022-12-17 10:10:22 +0300

    2

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -15,7 +15,7 @@ class PostAdmin(admin.ModelAdmin):
     list_editable = ('group',)
     search_fields = ('text',)
     list_filter = ('pub_date',)
-    empty_value_display = '-пуcто-'
+    empty_value_display = '-пусто-'
 
 
 class GroupAdmin(admin.ModelAdmin):
